2fe44333758d70aa8e105217c437937677d0f68a
Sergey Monakhov <monakhov@puzl.ee>
2020-09-03 14:21:24 +0300

    update ci

--- deploy/run_publish.sh
+++ deploy/run_publish.sh
@@ -4,7 +4,7 @@ repo_path="$(git rev-parse --show-toplevel)"
 echo "repo_path: ${repo_path}"
 origin_commit="$(git rev-parse --short HEAD)"
 echo "origin_commit: ${origin_commit}"
-files_to_push="$(git diff-tree --no-commit-id --name-only -r \"${origin_commit}^\")"
+files_to_push="$(git diff-tree --no-commit-id --name-only -r ${origin_commit}^)"
 echo "files_to_push: ${files_to_push}"
 
 . "${repo_path}/deploy/functions/publish.sh"
